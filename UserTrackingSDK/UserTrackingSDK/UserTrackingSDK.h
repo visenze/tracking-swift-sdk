14fde9828039ae21b15ade367689e93b2a2d5051
Chen Xiaoman <836932889@qq.com>
2018-05-22 16:41:31 +0800

    Create new project

---
+++ UserTrackingSDK/UserTrackingSDK/UserTrackingSDK.h
@@ -0,0 +1,19 @@
+//
+//  UserTrackingSDK.h
+//  UserTrackingSDK
+//
+//  Created by visenze on 22/5/18.
+//  Copyright © 2018 ViSenze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UserTrackingSDK.
+FOUNDATION_EXPORT double UserTrackingSDKVersionNumber;
+
+//! Project version string for UserTrackingSDK.
+FOUNDATION_EXPORT const unsigned char UserTrackingSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserTrackingSDK/PublicHeader.h>
+
+
